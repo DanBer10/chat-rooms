83b95108bba3a1548fcbf30ff1c1c7ad2acd3d50
DanBer10 <dannebernhardt@outlook.com>
2021-08-12 16:44:45 +0200

    add channel to backend firebase working

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -2,6 +2,17 @@ import React from "react";
 import styled from "styled-components";
 import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
 import CreateIcon from "@material-ui/icons/Create";
+import SideBarOptions from "./SideBarOptions";
+import InsertCommentIcon from "@material-ui/icons/InsertComment";
+import InboxIcon from "@material-ui/icons/Inbox";
+import DraftsIcon from "@material-ui/icons/Drafts";
+import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
+import FileCopyIcon from "@material-ui/icons/FileCopy";
+import PeopleAltIcon from "@material-ui/icons/PeopleAlt";
+import AppsIcon from "@material-ui/icons/Apps";
+import ExpandLessIcon from "@material-ui/icons/ExpandLess";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import AddIcon from "@material-ui/icons/Add";
 
 function SideBar() {
   return (
@@ -16,6 +27,20 @@ function SideBar() {
         </SideBarInfo>
         <CreateIcon />
       </SideBarHeader>
+
+      <SideBarOptions Icon={InsertCommentIcon} title="Threads" />
+      <SideBarOptions Icon={InboxIcon} title="Mentions & reactions" />
+      <SideBarOptions Icon={DraftsIcon} title="Saved items" />
+      <SideBarOptions Icon={BookmarkBorderIcon} title="Channel browser" />
+      <SideBarOptions Icon={PeopleAltIcon} title="People & user groups" />
+      <SideBarOptions Icon={AppsIcon} title="Apps" />
+      <SideBarOptions Icon={FileCopyIcon} title="File Browser" />
+      <SideBarOptions Icon={ExpandLessIcon} title="Show less" />
+      <SideBarOptions Icon={ExpandMoreIcon} title="Show More" />
+      <hr />
+      <SideBarOptions Icon={ExpandMoreIcon} title="Channels" />
+      <hr />
+      <SideBarOptions Icon={AddIcon} addChannelOption title="Add channel" />
     </SidebarContainer>
   );
 }
@@ -30,6 +55,12 @@ const SidebarContainer = styled.div`
   max-width: 260px;
   margin-top: 60px;
   height: 100vh;
+
+  > hr {
+    margin-top: 10px;
+    margin-bottom: 10px;
+    border: 1px solid #49274b;
+  }
 `;
 
 const SideBarHeader = styled.div`
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyDVKbpYpEailpBbYSCh9gmtljDOox6DOO8",
   authDomain: "chat-c78a8.firebaseapp.com",
@@ -6,3 +8,10 @@ const firebaseConfig = {
   messagingSenderId: "948965231082",
   appId: "1:948965231082:web:55cc1b9fab9ea5334eca93",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { db, auth, provider };
