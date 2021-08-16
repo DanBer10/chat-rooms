92ed886a30ca380823f20ea879a5d610d42dcc12
DanBer10 <dannebernhardt@outlook.com>
2021-08-16 17:02:01 +0200

    sign in with google working

--- src/App.js
+++ src/App.js
@@ -5,20 +5,30 @@ import Header from "./components/Header";
 import styled from "styled-components";
 import SideBar from "./components/SideBar";
 import Chat from "./components/Chat";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "./firebase";
+import Login from "./components/Login";
+
 function App() {
+  const [user, loading] = useAuthState(auth);
+
   return (
     <Router>
-      <>
-        <Header />
-        <AppBody>
-          <SideBar />
-          <Switch>
-            <Route path="/" exact>
-              <Chat />
-            </Route>
-          </Switch>
-        </AppBody>
-      </>
+      {!user ? (
+        <Login />
+      ) : (
+        <>
+          <Header />
+          <AppBody>
+            <SideBar />
+            <Switch>
+              <Route path="/" exact>
+                <Chat />
+              </Route>
+            </Switch>
+          </AppBody>
+        </>
+      )}
     </Router>
   );
 }
